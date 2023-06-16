266336aee29fe8885265767777d9a907f22001b8
BillionaireNerd <svylaw@gmail.com>
2023-06-16 00:36:33 +0000

    betty hmmm

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - first function
+ *
+ * Return: 0 because i no to get
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,");
+	return (0);
+}
