9a5e0e1e26b92660a00c5bc5efb30d59f1854b7c
BillionaireNerd <svylaw@gmail.com>
2023-06-17 04:29:41 +0000

    f

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - no argument
+ *
+ * Return: 0 success
+ */
+int main(void)
+{
+	int d;
+
+	for (d = 0; d <= 9; d++)
+	{
+		printf("%d", d);
+	}
+	printf("\n");
+
+	return (0);
+}
