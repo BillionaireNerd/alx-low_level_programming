a08311c5079da5608ba88a5bb453b0dc2d287949
BillionaireNerd <svylaw@gmail.com>
2023-06-17 04:38:00 +0000

    g

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - no arg
+ *
+ * Return: 0 success
+ */
+int main(void)
+{
+	int d;
+
+	for (d = '0'; d <= '9'; d++)
+	{
+		putchar(d);
+	}
+	putchar('\n');
+
+	return (0);
+}
